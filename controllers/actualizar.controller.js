69c0e34c8b869c0387dbcf40a5981b624af8dd84
DavidRR <druidiasr@gmail.com>
2022-11-08 17:23:56 -0500

    proyecto completado

--- controllers/actualizar.controller.js
+++ controllers/actualizar.controller.js
@@ -2,21 +2,28 @@ import { clientServices } from "../service/client-service.js";
 
 const formulario = document.querySelector("[data-form]");
 
-const obtenerInformacion = () => {
-    const url =new URL(window.location);
-    const id = url.searchParams.get("id");
+const obtenerInformacion = async () => {
+   const url =new URL(window.location);
+   const id = url.searchParams.get("id");
 
-    if(id == null) {
+   if(id == null) {
        window.location.href = "/screens/error.html";
-    }
+   }
 
-    const nombre = document.querySelector("[data-nombre]");
-    const email = document.querySelector("[data-email]");
+   const nombre = document.querySelector("[data-nombre]");
+   const email = document.querySelector("[data-email]");
 
-    clientServices.detalleCliente(id).then((perfil) => {
-       nombre.value = perfil.nombre;
-       email.value = perfil.email; 
-    });
+   try {
+      const perfil = await clientServices.detalleCliente(id);
+      if (perfil.nombre && perfil.email) {
+         nombre.value = perfil.nombre;
+         email.value = perfil.email; 
+      }  else {
+         throw new Error();
+      }
+   }  catch (error) {
+      windows.location.href = "/screens/error.html";
+   }
 };
 
 obtenerInformacion();
